75e7785830441c7cb84cba9256078abd852ba333
649713977@qq.com <649713977@qq.com>
2022-11-13 22:00:42 +0800

    first commit

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
 <?php
 require_once 'core/run.php';
-/**
-require 'core/view/ViewDriver.php';
-require 'core/view/Taglib.php';
-require 'core/view/Tag.php';
-require 'core/view/Template.php';
-require 'core/view/View.php';
-$v = new View();
-$a = [
-    ["a"=>'b'],
-    ['a'=>'e'],
-    ['a'=>'d']
-];
-$v->assign("cc",$a);
-$v->fetch("c/a.html");
-**/
